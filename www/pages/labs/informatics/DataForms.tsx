257c2781f6472da10057d8f659163341fbf53eba
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2022-07-21 16:59:07 +0300

    +Data forms notes

---
+++ www/pages/labs/informatics/DataForms.tsx
@@ -0,0 +1,16 @@
+export const Caption = "Формы данных";
+export const Id = "data-forms";
+
+export const Section = () => (
+    <section>
+        <h2 id="{Id}">{Caption}</h2>
+
+        <p>
+            TODO:Mesh
+        </p>
+
+        <p>
+            TODO:Стек, очередь - структуры данных или интерфейсы?
+        </p>
+    </section>
+);
